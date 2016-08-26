cd6f08e9c8ebb0ec8ebd0672866daec7e918c733
rhondarobot <rrachellej@gmail.com>
2016-08-26 15:20:42 -0700

    still saying undefined but not showing extra quotation mark2

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(function () {
 function getRequest(searchTerm){
   var params = {
     part: 'snippet',
-    maxResults: 9, 
+    maxResults: 8, 
     key: 'AIzaSyCo89pj5c21WfnyiC_LBSb9prU4oUu_o-w',
     q: searchTerm,
     type: 'video'
@@ -18,14 +18,14 @@ function getRequest(searchTerm){
 
   $.getJSON(url, params, function(data){
     showResults(data.items);
-
+    console.log(data.items);
   });
 }
 
 
 function showResults(results){
   $.each(results, function(index,value){
-    $('.search-results').append('<li class="results">' + value.snippet.title + '<br/>' + '<a class="videoLink" href="https://youtube.com/watch?v=' + value.id.videoID + '"><img class="thumbnail" src="' +value.snippet.thumbnails.medium.url+ '"></a>' + '<br/>' + value.snippet.description + '</li>');
+    $('.search-results').append('<li class="results">' + value.snippet.title + '<br/>' + '<a href="https://www.youtube.com/watch?v=' + value.id.videoID + '"><img class="thumbnail" src="' +value.snippet.thumbnails.medium.url+ '"></a>' + '<br/>' + value.snippet.description + '</li>');
     console.log(value.snippet.title);
   });
 }
