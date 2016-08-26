4ffd0f2f225c2927d5b7c5ca31dc88b74fde7753
rhondarobot <rrachellej@gmail.com>
2016-08-26 12:43:08 -0700

    links not linking to video id link

--- js/app.js
+++ js/app.js
@@ -27,16 +27,9 @@ function showResults(results){
   var html = "";
   console.log(results);
   $.each(results, function(index,value){
-    $('.search-results').append('<li class="results">' + value.snippet.title + '<a href class="videoLink" ="https://youtube.com/watch?v=' + value.id.videoID + '"><img class="thumbnail" src=" ' + value.snippet.thumbnails.medium.url + '">' + '</a>' + value.snippet.description + '</li>');
+    $('.search-results').append('<li class="results">' + value.snippet.title + '<a href class="videoLink" ="https://youtube.com/watch?v=' + value.id.videoID + '"><img class="thumbnail" src=" ' + value.snippet.thumbnails.medium.url + '"></a>' + value.snippet.description + '</li>');
     console.log(value.snippet.title);
     console.log(value.snippet.thumbnails.medium.url);
   });
 }
 
-// function showVideos(videos) {
-//   // console.log(videos);
-//   $.each(videos, function(index, value){ 
-//     $('.thumbnail-container').append('<li><img class="thumbnail" src="' + value.snippet.thumbnails.high.url + '"><a href class="videoLink" ="https://www.youtube.com/watch?v='+ value.id.videoId + '">' + value.snippet.title + '</a></li>'); 
-//     // console.log(value.snippet.thumbnails.medium.url); 
-//   }); 
-// }
